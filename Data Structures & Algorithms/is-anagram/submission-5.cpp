2dccea28ebeed3a10c49f56beaaf2ae0d2cfe9b0
psurge1 <suraj.sswamy@gmail.com>
2026-04-13 12:16:04 -0500

    Add: is-anagram - submission-5

---
+++ Data Structures & Algorithms/is-anagram/submission-5.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if (s.size() != t.size()) {
+            return false;
+        }
+
+        vector<int> charCount(26, 0);
+        for (int i = 0; i < s.size(); ++i) {
+            --charCount[s[i] - 'a'];
+            ++charCount[t[i] - 'a'];
+        }
+
+        for (int i = 0; i < 26; ++i) {
+            if (charCount[i] != 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
