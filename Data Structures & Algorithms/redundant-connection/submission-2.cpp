9ce5a229a33b05703c3e3d500dd23827036683ea
psurge1 <suraj.sswamy@gmail.com>
2026-05-24 19:15:42 -0500

    Add: redundant-connection - submission-2

---
+++ Data Structures & Algorithms/redundant-connection/submission-2.cpp
@@ -0,0 +1,43 @@
+class UnionFind {
+private:
+    vector<int> parent;
+public:
+    UnionFind(size_t n) : parent(n) {
+        for (int i = 0; i < n; ++i) {
+            parent[i] = i;
+        }
+    }
+
+    int findRoot(int node) {
+        if (parent[node] == node) {
+            return node;
+        }
+        parent[node] = findRoot(parent[node]);
+        return parent[node];
+    }
+
+    bool joinedNodes(int nodeOne, int nodeTwo) {
+        int rootOne = findRoot(nodeOne);
+        int rootTwo = findRoot(nodeTwo);
+        if (rootOne == rootTwo) {
+            return true;
+        }
+        parent[rootOne] = rootTwo;
+        return false;
+    }
+};
+
+class Solution {
+public:
+    vector<int> findRedundantConnection(vector<vector<int>>& edges) {
+        UnionFind dset(edges.size());
+        for (const vector<int>& edge : edges) {
+            bool alreadyConnected = dset.joinedNodes(edge[0] - 1, edge[1] - 1);
+            if (alreadyConnected) {
+                // last edge of cycle found
+                return edge;
+            }
+        }
+        return {};
+    }
+};
