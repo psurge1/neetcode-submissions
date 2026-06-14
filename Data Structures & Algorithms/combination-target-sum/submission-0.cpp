db1950666a61a52a766e3ed359d40e99f53465df
psurge1 <suraj.sswamy@gmail.com>
2026-06-14 14:26:23 -0500

    Add: combination-target-sum - submission-0

---
+++ Data Structures & Algorithms/combination-target-sum/submission-0.cpp
@@ -0,0 +1,28 @@
+class Solution {
+private:
+    void genCSums(vector<int>& currList, vector<vector<int>>& cSums, int idx, int target, const vector<int>& nums) {
+        if (target < 0) {
+            return;
+        }
+        if (idx == nums.size()) {
+            if (target == 0) {
+                cSums.push_back(currList);
+            }
+        }
+        else {
+            genCSums(currList, cSums, idx + 1, target, nums);
+
+            currList.push_back(nums[idx]);
+            genCSums(currList, cSums, idx, target - nums[idx], nums);
+            currList.pop_back();
+        }
+    }
+public:
+    vector<vector<int>> combinationSum(vector<int>& nums, int target) {
+        vector<vector<int>> cSums;
+        vector<int> currList;
+        genCSums(currList, cSums, 0, target, nums);
+
+        return cSums;
+    }
+};
