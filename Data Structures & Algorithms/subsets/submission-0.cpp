5163c282a45d746e8f90fbbbf25e150517fa73be
psurge1 <suraj.sswamy@gmail.com>
2026-06-14 13:53:31 -0500

    Add: subsets - submission-0

---
+++ Data Structures & Algorithms/subsets/submission-0.cpp
@@ -0,0 +1,27 @@
+class Solution {
+private:
+    void findSubsets(vector<int>& subset, vector<vector<int>>& subsets, int idx, const vector<int>& nums) {
+        if (idx == nums.size()) {
+            subsets.push_back(subset);
+        }
+        else {
+            // dont take this number
+            findSubsets(subset, subsets, idx + 1, nums);
+
+            subset.push_back(nums[idx]);
+            findSubsets(subset, subsets, idx + 1, nums);
+            subset.pop_back();
+        }
+    }
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        /*
+        at every step, either choose to take a number or dont
+        */
+
+        vector<vector<int>> subsets;
+        vector<int> subset;
+        findSubsets(subset, subsets, 0, nums);
+        return subsets;
+    }
+};
