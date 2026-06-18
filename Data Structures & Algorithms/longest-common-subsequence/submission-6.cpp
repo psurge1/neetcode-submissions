69ed5f8656178e181558c3e335c516429d5e006d
psurge1 <suraj.sswamy@gmail.com>
2026-06-18 18:37:49 -0500

    Add: longest-common-subsequence - submission-6

---
+++ Data Structures & Algorithms/longest-common-subsequence/submission-6.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int longestCommonSubsequence(string text1, string text2) {
+        int sz1 = text1.size();
+        int sz2 = text2.size();
+
+        // opt (i, j) gives the longest subsequence considering the first i characters of text1 and first j characters of text2 
+        vector<vector<int>> opt(sz1 + 1, vector<int>(sz2 + 1, -1));
+        for (int i = 0; i <= sz1; ++i) {
+            opt[i][0] = 0;
+        }
+        for (int i = 0; i <= sz2; ++i) {
+            opt[0][i] = 0;
+        }
+
+        for (int i = 1; i <= sz1; ++i) {
+            for (int j = 1; j <= sz2; ++j) {
+                if (text1[i - 1] == text2[j - 1]) {
+                    opt[i][j] = 1 + opt[i - 1][j - 1];
+                }
+                else {
+                    opt[i][j] = max(opt[i - 1][j], opt[i][j - 1]);
+                }
+            }
+        }
+
+        for (auto a : opt) {
+            for (auto b : a) {
+                cout << b << " ";
+            }
+            cout << "\n";
+        }
+
+        return opt[sz1][sz2];
+    }
+};
