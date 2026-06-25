d8ccd66cdbc0dd04f6bc4a4c9d1a74f08279dec2
psurge1 <suraj.sswamy@gmail.com>
2026-06-24 20:56:06 -0500

    Add: interleaving-string - submission-11

---
+++ Data Structures & Algorithms/interleaving-string/submission-11.py
@@ -0,0 +1,26 @@
+class Solution:
+    def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
+        s1_size = len(s1)
+        s2_size = len(s2)
+        if len(s3) != s1_size + s2_size:
+            return False
+        
+        table = [[False] * (s2_size + 1) for _ in range(s1_size + 1)]
+
+        for idx1 in range(s1_size + 1):
+            for idx2 in range(s2_size + 1):
+                if idx1 == 0 and idx2 == 0:
+                    table[idx1][idx2] = True
+                elif idx1 == 0:
+                    table[idx1][idx2] = (s2[idx2 - 1] == s3[idx2 - 1] and table[idx1][idx2 - 1])
+                elif idx2 == 0:
+                    table[idx1][idx2] = (s1[idx1 - 1] == s3[idx1 - 1] and table[idx1 - 1][idx2])
+                else:
+                    if s1[idx1 - 1] == s3[idx1 + idx2 - 1] and table[idx1 - 1][idx2]:
+                        table[idx1][idx2] = True
+                    elif s2[idx2 - 1] == s3[idx1 + idx2 - 1] and table[idx1][idx2 - 1]:
+                        table[idx1][idx2] = True
+                    else:
+                        table[idx1][idx2] = False
+        
+        return table[s1_size][s2_size]
