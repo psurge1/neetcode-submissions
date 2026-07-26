828f3be3e26b04495845ebb37570624f381a2678
psurge1 <suraj.sswamy@gmail.com>
2026-07-26 13:10:23 -0500

    Add: valid-sudoku - submission-1

---
+++ Data Structures & Algorithms/valid-sudoku/submission-1.py
@@ -0,0 +1,61 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        height = 9
+        width = 9
+        boxwidth = 3
+        def validRows():
+            for row in range(height):
+                alreadyExists = [False] * 10
+                for col in range(width):
+                    if board[row][col] == ".":
+                        continue
+                    num = int(board[row][col])
+                    if alreadyExists[num]:
+                        return False
+                    alreadyExists[num] = True
+            return True
+
+        def validCols():
+            for col in range(width):
+                alreadyExists = [False] * 10
+                for row in range(height):
+                    if board[row][col] == ".":
+                        continue
+                    num = int(board[row][col])
+                    if alreadyExists[num]:
+                        return False
+                    alreadyExists[num] = True
+            return True
+        
+        def validSubBoxes():
+            for box in range(9):
+                cornerRow = (box // 3) * 3
+                cornerCol = (box % 3) * 3
+                alreadyExists = [False] * 10
+                for row in range(cornerRow, cornerRow + boxwidth):
+                    for col in range(cornerCol, cornerCol + boxwidth):
+                        if board[row][col] == ".":
+                            continue
+                        num = int(board[row][col])
+                        if alreadyExists[num]:
+                            print(box, num, alreadyExists)
+                            return False
+                        alreadyExists[num] = True
+            return True
+            
+        """
+        box 0, 1, 2, 3, 4, 5, 6, 7, 8
+        col 0, 3, 6, 0, 3, 6, 0, 3, 6 = (box % 3) * 3
+        row 0, 0, 0, 3, 3, 3, 6, 6, 6 = (box // 3) * 3
+        """
+        
+        if not validRows():
+            print("ROW")
+            return False
+        if not validCols():
+            print("COL")
+            return False
+        if not validSubBoxes():
+            print("BOX")
+            return False
+        return True
