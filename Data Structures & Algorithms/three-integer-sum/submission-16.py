e87e7e7f2659917368f383f152bc812809cb5393
psurge1 <suraj.sswamy@gmail.com>
2026-07-27 22:17:33 -0500

    Add: three-integer-sum - submission-16

---
+++ Data Structures & Algorithms/three-integer-sum/submission-16.py
@@ -0,0 +1,32 @@
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        solution = []
+        numsLen = len(nums)
+        nums.sort()
+        idx = 0
+        while idx < numsLen:
+            seenNums = set()
+            jdx = idx + 1
+            while jdx < numsLen:
+                target = -(nums[idx] + nums[jdx])
+                if target in seenNums:
+                    solution.append([nums[idx], nums[jdx], target])
+                    jdx += 1
+                    # if we find a solution using a specific jdx, we dont want to duplicate that solution so we increment it until we reach a new number
+                    # we dont want to do this during the "solution not found" path because we will skip over solutions that may use duplicate but non-repeated (different indeces) numbers
+                    while jdx < numsLen and nums[jdx] == nums[jdx - 1]:
+                        jdx += 1
+                else:
+                    seenNums.add(nums[jdx])
+                    jdx += 1
+                
+                # the purpose of this set is to track the "middle" element of a triplet
+                # we want to check the sum of nums[idx] and nums[jdx] against numbers already visited
+                # nums[idx] <= numbers in set < nums[jdx]
+                
+                
+            idx += 1
+            while idx < numsLen and nums[idx] == nums[idx - 1]:
+                idx += 1
+        
+        return solution
