ac8eaf8825f387a7c9e55e281174d52134eccfd5
psurge1 <suraj.sswamy@gmail.com>
2026-09-11 13:54:05 -0500

    Add: serialize-and-deserialize-binary-tree - submission-0

---
+++ Data Structures & Algorithms/serialize-and-deserialize-binary-tree/submission-0.py
@@ -0,0 +1,64 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Codec:
+    
+    # Encodes a tree to a single string.
+    def serialize(self, root: Optional[TreeNode]) -> str:
+        res = []
+
+        queue = deque()
+        queue.append(root)
+        while len(queue) > 0:
+            node = queue.popleft()
+            if node is None:
+                res.append("#")
+            else:
+                res.append(str(node.val))
+                queue.append(node.left)
+                queue.append(node.right)
+
+        return ",".join(res)
+        
+    # Decodes your encoded data to tree.
+    def deserialize(self, data: str) -> Optional[TreeNode]:
+        node_items = deque(data.split(","))
+
+        if node_items[0] == "#":
+            return None
+        
+        num_nodes = len(node_items)
+        """
+        getting stuck on how to convert a level-order queue to a tree
+        level order means all nodes of the same level are adjacent
+
+        Steps
+        - create a node
+        - add it to a populate_children queue
+        - when you pop from populate_children, pop 2 from node_items to create 2 children
+
+        we know that every non null (not #) node will have 2 children (possibly null) as well
+        this means that for every node in populate_children, there must be 2 children in node_items
+        """
+        head = TreeNode(int(node_items[0]))
+        node_items.popleft()
+
+        populate_children = deque()
+        populate_children.append(head)
+
+        while len(populate_children) > 0:
+            parent_node = populate_children.popleft()
+            lc_val = node_items.popleft()
+            rc_val = node_items.popleft()
+            if lc_val != "#":
+                parent_node.left = TreeNode(int(lc_val))
+                populate_children.append(parent_node.left)
+            if rc_val != "#":
+                parent_node.right = TreeNode(int(rc_val))
+                populate_children.append(parent_node.right)
+
+        return head
